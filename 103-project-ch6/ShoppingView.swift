d8a5021461cddff962435eb8432b22977e43af3f
Tatiana Mccray <ejamison06@gmail.com>
2026-01-29 15:58:00 -0600

    103/2

---
+++ 103-project-ch6/ShoppingView.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingView.swift
+//  103-project-ch6
+//
+//  Created by Tatiana6mo on 1/27/26.
+//
+
+import SwiftUI
+
+struct ShoppingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShoppingView()
+}
