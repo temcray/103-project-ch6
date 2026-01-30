1829781259d2c325cc77209c70a694032be2f57c
Tatiana Mccray <ejamison06@gmail.com>
2026-01-29 18:08:24 -0600

    103/maybe done

---
+++ 103-project-ch6/playgrounds/assignments/StudentView.swift
@@ -0,0 +1,18 @@
+//
+//  StudentView.swift
+//  103-project-ch6
+//
+//  Created by Tatiana6mo on 1/29/26.
+//
+
+import SwiftUI
+
+struct StudentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentView()
+}
