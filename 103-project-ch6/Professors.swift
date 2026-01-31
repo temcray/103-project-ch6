87fb76091e947e2e2e0398979cfdb314a22aacf6
Tatiana Mccray <ejamison06@gmail.com>
2026-01-31 13:33:50 -0600

    103/4

---
+++ 103-project-ch6/Professors.swift
@@ -0,0 +1,18 @@
+//
+//  Professors.swift
+//  103-project-ch6
+//
+//  Created by Tatiana6mo on 1/31/26.
+//
+
+import SwiftUI
+
+struct Professors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Professors()
+}
---
+++ 103-project-ch6/models/Proessor.swift
@@ -0,0 +1,21 @@
+//
+//  Proessor.swift.swift
+//  103-project-ch6
+//
+//  Created by Tatiana6mo on 1/31/26.
+//
+import Foundation
+class Professor {
+    var firstName: String
+    var lastName: String
+    var phoneNumber: String
+    
+    init (firstName: String, lastName: String, phoneNumber: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.phoneNumber = phoneNumber
+    }
+}
+
+
+
